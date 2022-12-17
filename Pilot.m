4c3738f09256908867207f7e09691a6bd590f3b8
AdelHashem <96683697+AdelHashem@users.noreply.github.com>
2022-12-17 17:02:04 +0200

    Create Pilot.m
    Estimate the Multi-path fading channel

---
+++ Pilot.m
@@ -0,0 +1,16 @@
+function H_est = Pilot(h,EbNo,Nfft,cp,M)
+symbols(1:1024) = 3+3i;
+
+sig_ifft = ifft(symbols,Nfft);
+
+sig = cpAdd(sig_ifft,cp);
+
+r = channel(sig,h,EbNo,M);
+
+r = cpRem(r,cp,Nfft);
+
+H_est = fft(r)./ symbols;
+
+
+end
+
