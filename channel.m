2195469a18279f036bc9d38dba776ec260df5079
AdelHashem <96683697+AdelHashem@users.noreply.github.com>
2022-12-17 16:59:37 +0200

    Create channel.m
    Create channel Fun to conv the Fading impulse channel with the signal then add AWGN

---
+++ channel.m
@@ -0,0 +1,8 @@
+function [output] = channel(signal,h,EbNodb,M)
+SNR = EbNodb + 10*log10(log2(M));
+%h = (1/sqrt(2*L)) * (randn(1,L) + 1i * rand(1,L));
+signal = conv(h,signal);
+noise = awgn(signal,SNR,'measured');
+output = noise;
+end
+
