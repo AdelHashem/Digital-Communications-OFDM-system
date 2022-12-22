c1f3d6d09dcbd5a43aa65c2b769906fdd13ae5ea
Abdallah <72445656+abdallah-ghanem@users.noreply.github.com>
2022-12-23 00:40:36 +0200

    Update Modulation.m

--- Modulation.m
+++ Modulation.m
@@ -86,6 +86,81 @@ classdef Modulation
                     
                     
                     
+        end
+                
+        function out = QAM64(obj,data,selector)
+            % Mod & Demod The 16QAM
+            % i/p: selector ['Mod','Demod']
+            MapTable = [0 0 0 0 0 0;0 0 0 0 0 1;0 0 0 0 1 0;0 0 0 0 1 1;
+            0 0 0 1 0 0;0 0 0 1 0 1;0 0 0 1 1 0;0 0 0 1 1 1;
+            0 0 1 0 0 0;0 0 1 0 0 1;0 0 1 0 1 0;0 0 1 0 1 1;
+            0 0 1 1 0 0;0 0 1 1 0 1;0 0 1 1 1 0;0 0 1 1 1 1];
+            Map = 1/sqrt(42).*[-7-7j -7-5j -7-1j -7-3j 
+                               -7+7j -7+5j -7+1j -7+3j
+                               -5-7j -5-5j -5-1j -5-3j
+                               -5+7j -5+5j -5+1j -5+3j
+                               -1-7j -1-5j -1-1j -1-3j
+                               -1+7j -1+5j -1+1j -1+3j
+                               -3-7j -3-5j -3-1j -3-3j
+                               -3+7j -3+5j -3+1j -3+3j
+                                7-7j 7-5j 7-1j 7-3j
+                                7+7j 7+5j 7+1j 7+3j
+                                5-7j 5-5j 5-1j 5-3j
+                                5+7j 5+5j 5+1j 5+3j
+                                1-7j 1-5j 1-1j 1-3j
+                                1+7j 1+5j 1+1j 1+3j
+                                3-7j 3-5j 3-1j 3-3j
+                                3+7j 3+5j 3+1j 3+3j];
+       
+            if selector == "Mod"
+                data = obj.splitdata(data,6);
+                out = Map(data+1);
+            elseif selector == "Demod"
+                out = [];
+                for k = 1:1:length(data)
+                    be = real(data(k));
+                    bo = imag(data(k));
+                    if (be > 6/(sqrt(42)))
+                        be = [1 0 0];
+                    elseif (be > 4/(sqrt(42))&& be < 6/(sqrt(42)))
+                        be = [1 0 1];
+                    elseif (be > 2/(sqrt(42)) && be < 4/(sqrt(42)))
+                        be = [1 1 1];    
+                    elseif (be > 0 && be < 2/(sqrt(42)))
+                        be = [1 1 0];
+                    elseif (be < -2/(sqrt(42)) && be < 0)
+                        be = [0 1 0];
+                    elseif (be < -4/(sqrt(42)) && be > -2/(sqrt(42)))
+                        be = [0 1 1];
+                    elseif (be < -6/(sqrt(42)) && be > -4/(sqrt(42)))
+                        be = [0 0 1];    
+                    else
+                        be = [0 0 0];
+                    end
+                    if (bo > 6/(sqrt(42)))
+                        bo = [1 0 0];
+                    elseif (bo > 4/(sqrt(42)) && bo < 6/(sqrt(42)))
+                        bo = [1 0 1];
+                    elseif (bo > 2/(sqrt(42)) && bo < 4/(sqrt(42)))
+                        bo = [1 1 1];    
+                    elseif (bo > 0 && bo < 2/(sqrt(42)))
+                        bo = [1 1 0];
+                    elseif (bo < -2/(sqrt(42)) && bo < 0)
+                        bo = [0 1 0];
+                    elseif (bo < -4/(sqrt(42)) && bo > -2/(sqrt(42)))
+                        bo = [0 1 1];
+                    elseif (bo < -6/(sqrt(42)) && bo > -4/(sqrt(42)))
+                        bo = [0 0 1];    
+                    else
+                        bo = [0 0 0];
+                    end
+                    b=[be bo];
+                    out =[out b];    
+                end
+            end
+                    
+                    
+                    
                 end
                 
                 
