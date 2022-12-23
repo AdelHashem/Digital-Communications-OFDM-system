83436abbb3b94236e16f9992de69d6a1bf90af69
AdelHashem <96683697+AdelHashem@users.noreply.github.com>
2022-12-23 22:49:50 +0200

    Update channel.m

--- channel.m
+++ channel.m
@@ -1,7 +1,7 @@
 function [output] = channel(signal,h,EbNodb,M)
 SNR = EbNodb + 10*log10(log2(M));
 %h = (1/sqrt(2*L)) * (randn(1,L) + 1i * rand(1,L));
-signal = conv(h,signal);
+signal = conv(h,signal);  % comment this line for AGWN Only
 noise = awgn(signal,SNR,'measured');
 output = noise;
 end
