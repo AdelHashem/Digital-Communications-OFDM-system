185a604fc06f718f94fa33057c2c4d411389290c
AdelHashem <96683697+AdelHashem@users.noreply.github.com>
2022-12-23 18:55:34 +0200

    Automate some cases

--- SIMO.m
+++ SIMO.m
@@ -3,11 +3,24 @@ clear variables;
 
 frames = 100; % frames / Eb/No value
 Nsymbols = 15; % OFDM Symbos / Frame
-M = 4; %QPSK
+M = 16; %QPSK
 L = 50;
 x = 0:3:40;
+coding = "LinearBlock";
 BER = [];
 c = 1;
+
+if M == 4
+    modu = "QPSK";
+elseif M == 16
+    modu = "16QAM";
+elseif M == 64
+    modu = "64QAM";
+else
+    modu = "QPSK";
+end
+
+
 for EbNo = 0:3:40
     error = 0;
     total = 0;
@@ -24,6 +37,6 @@ end
 
 figure
 plot(x,log10(BER),'b--o')
-title(['BER VS Eb/No ' 'LinearBlock - QPSK']);
+title(['BER VS Eb/No (SIMO) ' coding ' - ' modu ]);
 xlabel('Eb/No (db)')
 ylabel('BER (log10)')
