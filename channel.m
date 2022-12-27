1e89736cff8d2fb8bea5b91142ff4259e8551094
Adel Hashem <96683697+AdelHashem@users.noreply.github.com>
2022-12-27 19:06:13 +0300

    Re-Implement the function

--- channel.m
+++ channel.m
@@ -1,8 +1,13 @@
 function [output] = channel(signal,h,EbNodb,M)
-SNR = EbNodb + 10*log10(log2(M));
-%h = (1/sqrt(2*L)) * (randn(1,L) + 1i * rand(1,L));
 signal = conv(h,signal);  % comment this line for AGWN Only
-noise = awgn(signal,SNR,'measured');
-output = noise;
+
+Eb = 1 / log2(M);
+EbNo_linear=10^(EbNodb/10);
+% Calculating Noise PSD (No) corresponding to Eb/No
+No=Eb/EbNo_linear;
+Noise=sqrt(No/2)*(randn(length(signal),1)+1i*randn(length(signal),1));
+output = Noise' + signal ;
+
+
 end
 
