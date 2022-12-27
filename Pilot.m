83c541c71c61965018fcd22263ebc72a608d3751
Adel Hashem <96683697+AdelHashem@users.noreply.github.com>
2022-12-27 19:59:13 +0300

    normalize fft

--- Pilot.m
+++ Pilot.m
@@ -1,7 +1,7 @@
 function H_est = Pilot(h,EbNo,Nfft,cp,M)
 symbols(1:1024) = 3+3i;
 
-sig_ifft = ifft(symbols,Nfft);
+sig_ifft = ifft(symbols,Nfft).*sqrt(1024);
 
 sig = cpAdd(sig_ifft,cp);
 
@@ -9,7 +9,7 @@ r = channel(sig,h,EbNo,M);
 
 r = cpRem(r,cp,Nfft);
 
-H_est = fft(r)./ symbols;
+H_est = (fft(r)./sqrt(1024))./ symbols;
 
 
 end
